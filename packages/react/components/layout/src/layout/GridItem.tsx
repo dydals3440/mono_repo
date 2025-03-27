9f7f697232c44d792227a7343969239c4004e1bd
김용민 <dydals3440@gmail.com>
2025-03-27 19:46:17 +0900

    feat: Grid 컴포넌트 생성

---
+++ packages/react/components/layout/src/layout/GridItem.tsx
@@ -0,0 +1,54 @@
+import { Ref } from "react";
+import { GridItemProps } from "./types";
+import * as React from "react";
+import { clsx } from "clsx";
+import { StyleSprinkles } from "../core/style.css";
+import { extractSprinkleProps } from "../utils/properties";
+import { vars } from "@matthew/themes";
+
+// 특정 컴포넌트의 unControlled 사용할 수 있게 ref
+const GridItem = (props: GridItemProps, ref: Ref<HTMLElement>) => {
+  const {
+    as = "div",
+    children,
+    color,
+    background,
+    area,
+    colEnd,
+    colSpan,
+    colStart,
+    rowEnd,
+    rowSpan,
+    rowStart,
+  } = props;
+
+  return React.createElement(
+    as,
+    {
+      ...props,
+      ref,
+      className: clsx([
+        StyleSprinkles(
+          extractSprinkleProps(props, Array.from(StyleSprinkles.properties)),
+        ),
+        props.className,
+      ]),
+      style: {
+        gridArea: area,
+        gridColumnEnd: colEnd,
+        gridColumnStart: colStart,
+        gridColumn: colSpan,
+        gridRowEnd: rowEnd,
+        gridRowStart: rowStart,
+        gridRow: rowSpan,
+        color: vars.colors.$scale[color]?.[700] || color,
+        background: vars.colors.$scale[background]?.[100] || background,
+        ...props.style,
+      },
+    },
+    children,
+  );
+};
+
+const _GridItem = React.forwardRef(GridItem);
+export { _GridItem as GridItem };
